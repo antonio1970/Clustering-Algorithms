266070a32e10dea520f34af9f598ab372520c559
antonio1970 <antoniorodriguezandres70@gmail.com>
2019-02-11 16:40:04 +0100

    'code'

---
+++ hierachical clustering.R
@@ -0,0 +1,19 @@
+#-----------------Hierarchichal clustering----------#
+
+d2<-iris[,-c(5)]
+d2 <- scale(d2)
+
+## Ward Hierarchical Clustering
+
+dis <- c("euclidean","manhattan","minkowski")
+met <- c("single","complete","average","mcquitty","median")
+ 
+ d <- dist(d2, method = "euclidean")
+ fit <- hclust(d, method="average")
+ # # display dendogram
+ 
+ plot(fit) 
+ 
+## draw dendogram with red borders around the 2 clusters
+
+  rect.hclust(fit, k=2, border="red")
