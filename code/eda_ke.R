4acdeaf21ab4a68c418c2197b660d3ae6e77739e
antonio1970 <antoniorodriguezandres70@gmail.com>
2019-03-05 13:12:58 +0100

    'code'

--- code/eda_ke.R
+++ code/eda_ke.R
@@ -7,7 +7,7 @@ library(naniar)
 
 # read csv file
 
-knowledge <- read.csv2("../Proyecto investigacion/Nilufar datos de KE/Cluster datos/KE_data_1995-2017 final version (corrected).csv", header = TRUE, dec = ",", sep = ";")
+knowledge <- read.csv2("../data/KE_data_1995-2017 final version_cluster.csv", header = TRUE, dec = ",", sep = ";")
 
 # Select subset of countries
 
@@ -23,6 +23,7 @@ afcountries <- c ("Algeria",	"Ethiopia", "Niger", "Angola", "Gabon", "Nigeria","
   
 afdata <-subset(knowledge, knowledge$Country %in% afcountries)
 head (afdata)
+# write.xlsx(afdata, file= "afdata.xlsx", row.names = FALSE) #Without Java requirmentes (writexl, library)
 
 # Summary
 summary(afdata)
@@ -40,7 +41,6 @@ length(yearlist)
 # Number of missing values
 n_miss(knowledge)
 n_complete(knowledge)
-prop_miss(knowledge)
 prop_complete(knowledge)
 pct_miss(knowledge)
 
@@ -64,6 +64,24 @@ m = cor(knowledge[, c(3:14)], use ="pairwise")
 m
 corrplot(m)
 
+# Generate regional dummiess
+ca <- c("Angola", "Cameroon", "Cabo Verde", "Central African Republic", "Chad","Equatorial Guinea", "Eritrea", "Ethiopia",
+        "Gabon", "Sao Tome and Principe")
+knowledge$Country.centralafrica <- ifelse(knowledge$Country %in% ca,1,0)
+
+ea <- c ("Burundi", "Comoros", "Congo Democratic Republic", "Congo Republic", "Djibouti", "Kenya",
+         "Rwanda", "Seychelles", "Somalia", "Sudan", "Tanzania", "Uganda", "Zambia", "Zimbawe")
+
+knowledge$Country.eastafrica <- ifelse(knowledge$Country %in% ea,1,0)
+
+na <- c("Algeria", "Egypt", "Libya", "Mauritania", "Morocco", "Sudan", "Tunisia")
+knowledge$Country.northafrica <- ifelse(knowledge$Country %in% na,1,0)
+
+sa<-  c("Botswana", "Lesotho", "Madagascar", "Malawi", "Mauritius", "Mozambique", "Namibia", "South  Africa", "Swaziland")
+knowledge$Country.southafrica <- ifelse(knowledge$Country %in% sa,1,0)
+
+wa <- c("Benin", "Burkina Faso", "Cote d'Ivoire", "Gambia", "Guinea Bissau", "Ghana", "Guinea", "Liberia", "Mali", "Niger", "Nigeria", "Senegal", "Sierra Leone", "Togo")
+knowledge$Country.westafrica <- ifelse(knowledge$Country %in% wa,1,0)
 
 # Split the data per year, generate 23 data frames
 
