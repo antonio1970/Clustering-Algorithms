5722b3c81670b67304709db696143b582cf3020b
antonio1970 <antoniorodriguezandres70@gmail.com>
2019-03-11 10:05:37 +0100

    'code'

--- code/loops.R
+++ code/loops.R
@@ -1,11 +1,32 @@
-afdata1_imputed = afdata1
 
+# Imputation of some of the variables before using Knns
 
+# Generate a copy of the data
 
+afdata1_imputed <- afdata1
 
-for (i in 1:length(afdata1_imputed$FIXBI2))
+# Generate a loop for filling with zeros missing values for FIXBI2, and check the result
+
+n=length(afdata1_imputed$FIXBI2)
+i=1
+while (i<=n)
 {
-  if (is.na(afdata1$FIXBI2[i])) {
+  
+  if (is.na(afdata1_imputed$FIXBI2[i])) {
     afdata1_imputed$FIXBI2[i] <-0
+    i=i+1
+    next
+  }
+  
+  print(paste (afdata1_imputed$Country[i], " : ",afdata1_imputed$FIXBI2[i]))
+  #move to the next country
+  while (afdata1_imputed$Year[i]!=1995 & i<=n){
+    i=i+1
   }
 }
+
+
+
+
+
+
