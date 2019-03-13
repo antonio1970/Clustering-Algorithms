4b83c989cebd22efd966d4dd312add4bd5c87669
antonio1970 <antoniorodriguezandres70@gmail.com>
2019-03-13 12:13:43 +0100

    'code'

--- code/reading_kei.R
+++ code/reading_kei.R
@@ -4,3 +4,20 @@ kei <- read.csv("../Clustering-Algorithms/data/keindex.csv")
 kei$X= NULL
 colnames(kei) <- c("Country", "Year", "KEI_Index")
 kei<-kei[order(kei$Year),]
+
+
+# Merge two files with another column with the KEI index. Initial check
+
+class(afdata$Country)
+levels(afdata$Country)
+
+class(kei$Country)
+levels(kei$Country)
+
+# Both factors have differente levels
+
+
+combined <- sort(union(levels(afdata$Country), levels(kei$Country)))
+afdata_full <- left_join(mutate(afdata, Country=factor(Country, levels=combined)),
+               mutate(kei, Country=factor(Country, levels=combined)))
+write.csv(afdata_full, "afdata_full.csv")
