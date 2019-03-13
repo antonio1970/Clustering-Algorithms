271e596b4ad0482ae59e4743d1d229172811d012
antonio1970 <antoniorodriguezandres70@gmail.com>
2019-03-13 11:14:20 +0100

    'code'

--- code/reading_kei.R
+++ code/reading_kei.R
@@ -1,6 +1,6 @@
 # Read the KE Index and create a dataframe
 
-kei <- read.csv("../Clustering-Algorithms/keindex.csv")
+kei <- read.csv("../Clustering-Algorithms/data/keindex.csv")
 kei$X= NULL
 colnames(kei) <- c("Country", "Year", "KEI_Index")
 kei<-kei[order(kei$Year),]
